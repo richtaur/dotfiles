ccbcf0c21338f0cdb07d915e0b6af5f4da8ce20a
Matt Hackett <richtaur@gmail.com>
2011-01-28 00:10:25 -0800

    yay new stuff like ; = : and uhhh some plugin

--- bash_profile.sh
+++ bash_profile.sh
@@ -7,14 +7,20 @@ alias bashrc='/usr/bin/vim ~/.bash_profile; source $_'
 alias dev='cd ~/dev'
 alias gcam='git commit -a -m'
 alias glog='git log --graph --pretty=format:"%Cred%h%Creset %Cblue%an%d%Creset %s %Cgreen(%cr)%Creset" --date=relative'
-alias gpom='git push origin master'
 alias hasinternet='ping google.com'
 alias hosts='sudo vim /etc/hosts'
 alias httpconf='sudo vim /Applications/MAMP/conf/apache/httpd.conf'
 alias ip='ifconfig | grep 192'
 alias ll='ls -lah'
+alias playground='vim ~/dev/playground/index.html'
 alias sshsh='ssh richter@209.20.66.182'
 alias todo='vim ~/dev/TODO.md'
+
+# git
+alias gpom='git push origin master'
+alias gpull='git pull origin master'
+alias gpush='git push origin master'
+alias gstat='git status'
 
 # Applications
 alias telog='tail -f /Applications/MAMP/logs/php_error.log'
@@ -24,6 +30,9 @@ alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias yui-min='java -jar ~/dev/code/java/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar'
 
 # LDG
+alias devandroid='cd ~/dev/sdk/android-sdk-mac_x86/tools'
+alias devldg='cd ~/dev/lost_decade'
+alias flamberge='cd ~/dev/lost_decade/flamberge'
 alias ldg='cd ~/Dropbox/Lost\ Decade'
 alias onslaught='cd ~/dev/lost_decade/onslaught'
 
@@ -39,8 +48,9 @@ PS1="[\e[0;32m\]\u:\W\[\e[m\]]\[\e[1;32m\]>\[\e[m\] "
 
 export CLICOLOR="1"
 export EDITOR="/usr/bin/vim"
-# This PATH includes Java SDK 1.6
-export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/opt/local/include:/opt/local/apache2/include:/usr/local/include:/opt/android-sdk/tools:/opt/android-sdk/platforms/android-3/tools"
+#export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/opt/local/include:/opt/local/apache2/include:/usr/local/include:/opt/android-sdk/tools:/opt/android-sdk/platforms/android-3/tools"
+# This PATH includes depot_tools (for building Chromium)
+export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/opt/local/include:/opt/local/apache2/include:/usr/local/include:/opt/android-sdk/tools:/opt/android-sdk/platforms/android-3/tools:/Users/hackett/Downloads/home/chrome-svn/tarball/depot_tools"
 
 ###############################################################################
 # Macros
@@ -65,6 +75,8 @@ untargz() {
 # Raptr
 ###############################################################################
 
+# svn co svn+ssh://mom.raptr.com/gxl/svn/repos/raptr/branches/phoenix/alpha_6
+
 alias cassandra='/Users/hackett/dev/raptr/apache-cassandra-0.6.5/bin/cassandra -f'
 alias code-review='~/svn/raptrweb/upload.py --reviewers=geoff@raptr.com --send_mail'
 alias qa='ssh root@qa.raptr.com'
@@ -88,6 +100,10 @@ cp-prd() {
 	scp $1 root@heweb2:/gxl/prd/raptrweb/$1
 	scp $1 root@xoweb3:/gxl/prd/raptrweb/$1
 	scp $1 root@xoweb4:/gxl/prd/raptrweb/$1
+}
+
+cp-custom() {
+	scp $2 root@$1:/gxl/prd/raptrweb/$2
 }
 
 ###############################################################################
