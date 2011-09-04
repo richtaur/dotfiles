42b0bee0cf848b9a0a71551170bdceb191eb2bd4
Matt Hackett <richtaur@gmail.com>
2011-09-04 14:35:17 -0700

    hm maybe this will work. also testing committing from server

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ ln -s $CURRENT_DIRECTORY/bash_profile.sh ./.bash_profile
 ln -s $CURRENT_DIRECTORY/gvim.rc ./.gvimrc
 ln -s $CURRENT_DIRECTORY/vim.rc ./.vimrc
 ln -s $CURRENT_DIRECTORY/vim ./.vim
-ln -s $CURRENT_DIRECTORY/git.config ./.gitconfig
-source ./.bash_profile
+#ln -s $CURRENT_DIRECTORY/git.config ./.gitconfig
 cd $CURRENT_DIRECTORY
+source ~/.bash_profile
